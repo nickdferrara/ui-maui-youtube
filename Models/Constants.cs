fe100cbd571d32774e949b223f479f91201b1416
nickdferrara <nick.d.ferrara@gmail.com>
2023-06-11 14:24:27 -0400

    Make youtube service and base methods

---
+++ Models/Constants.cs
@@ -0,0 +1,18 @@
+﻿namespace ui_maui_youtube.Models
+{
+    public static class Constants
+    {
+        public static string ApplicationName = "YouTube";
+        public static string EmailAddress = @"youtube@gmail.com";
+        public static string ApplicationId = "nickdferrara.YouTube.App";
+        public static string ApiServiceURL = @"https://youtube.googleapis.com/youtube/v3/";
+        public static string ApiKey = @"*";
+
+
+        public static uint MicroDuration { get; set; } = 100;
+        public static uint SmallDuration { get; set; } = 300;
+        public static uint MediumDuration { get; set; } = 600;
+        public static uint LongDuration { get; set; } = 1200;
+        public static uint ExtraLongDuration { get; set; } = 1800;
+    }
+}
---
+++ Services/YoutubeService.cs
@@ -0,0 +1,54 @@
+﻿using Maui.Apps.Framework.Services;
+using System.Net;
+using ui_maui_youtube.IServices;
+using ui_maui_youtube.Models;
+
+namespace ui_maui_youtube.Services
+{
+    public class YoutubeService : RestServiceBase, IApiService
+    {
+        public YoutubeService(IConnectivity connectivity) : base(connectivity)
+        {
+            SetBaseURL(Constants.ApiServiceURL);
+        }
+
+        public async Task<VideoSearchResult> SearchVideos(string searchQuery, string nextPageToken = "")
+        {
+            var resourceUri = $"search?part=snippet&maxResults=10&type=video&key={Constants.ApiKey}&q={WebUtility.UrlEncode(searchQuery)}"
+                +
+                (!string.IsNullOrEmpty(nextPageToken) ? $"&pageToken={nextPageToken}" : "");
+
+            var result = await GetAsync<VideoSearchResult>(resourceUri);
+
+            return result;
+        }
+
+        public async Task<ChannelSearchResult> GetChannels(string channelIDs)
+        {
+            var resourceUri = $"channels?part=snippet,statistics&maxResults=10&key={Constants.ApiKey}&id={channelIDs}";
+
+            var result = await GetAsync<ChannelSearchResult>(resourceUri);
+
+            return result;
+        }
+
+        public async Task<YoutubeVideoDetail> GetVideoDetails(string videoID)
+        {
+            var resourceUri = $"videos?part=contentDetails,id,snippet,statistics&key={Constants.ApiKey}&id={videoID}";
+
+            var result = await GetAsync<VideoDetailsResult>(resourceUri);
+
+            return result.Items.First();
+        }
+
+        public async Task<CommentsSearchResult> GetComments(string videoID)
+        {
+            var resourceUri = $"commentThreads?part=snippet&maxResults=100&key={Constants.ApiKey}&videoId={videoID}";
+
+            var result = await GetAsync<CommentsSearchResult>(resourceUri);
+
+            return result;
+        }
+
+    }
+}
