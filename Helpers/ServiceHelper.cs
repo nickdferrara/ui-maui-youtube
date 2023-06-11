8b1866874fe8dcb746d94672be780731d81a2490
nickdferrara <nick.d.ferrara@gmail.com>
2023-06-11 13:40:35 -0400

    Make service helper and common enums

---
+++ Helpers/ServiceHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ui_maui_youtube.Helpers
+{
+    public static class ServiceHelpers
+    {
+
+        public static TService GetService<TService>() => //test
+            Current.GetService<TService>();
+
+        public static IServiceProvider Current =>
+#if WINDOWS10_0_17763_0_OR_GREATER
+			MauiWinUIApplication.Current.Services;
+#elif ANDROID
+        MauiApplication.Current.Services;
+#elif IOS || MACCATALYST
+			MauiUIApplicationDelegate.Current.Services;
+#else
+			null;
+#endif
+    }
+}
